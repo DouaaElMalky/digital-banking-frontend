aee91618781aacf84c2e6ebc2c0997922789f0f7
Douaa El Malky <douaaelmalky3@gmail.com>
2024-12-29 00:29:59 +0100

    Commit

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {CustomerService} from '../services/customer.service';
-import {Observable} from 'rxjs';
+import {catchError, Observable, throwError} from 'rxjs';
+import {Customer} from '../model/customer.model';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-customers',
@@ -11,11 +13,25 @@ import {Observable} from 'rxjs';
   styleUrl: './customers.component.css'
 })
 export class CustomersComponent implements OnInit{
-  customers! : Observable<any>;
+  customers! : Observable<Array<Customer>>;
   errorMessage! : string;
-  constructor(private customerService: CustomerService) {
-  }
+  searchFormGroup : FormGroup | undefined;
+  // searchFormGroup! : FormGroup; //avec le ! on n'aura pas besoin de faire le test au niveau du code html
+  constructor(private customerService: CustomerService, private fb: FormBuilder) { }
   ngOnInit() : void {
-    this.customers = this.customerService.getCustomers();
+    this.searchFormGroup = this.fb.group({
+      keyword : this.fb.control("")
+    })
+    this.handleSearchCustomers();
+  }
+
+  handleSearchCustomers() {
+    let kw = this.searchFormGroup?.value.keyword;
+    this.customers = this.customerService.searchCustomers(kw).pipe(
+      catchError(err => {
+        this.errorMessage = err.message;
+        return throwError(err);
+      })
+    );
   }
 }
