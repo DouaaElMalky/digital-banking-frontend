41e6b724d7b5bbd00ba64c896de05c08d8f8c412
Douaa El Malky <douaaelmalky3@gmail.com>
2024-12-29 14:15:20 +0100

    Commit

--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {AccountDetails} from '../model/account.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,8 @@ export class AccountsService {
 
   constructor(private http : HttpClient) { }
 
-  public getAccount(accountId : string){
-    return this.http.get(environment.backendHost+"/accounts/"+accountId+"/pageOperations");
+  public getAccount(accountId : string, page : number, size : number):Observable<AccountDetails>{
+    return this.http.get<AccountDetails>(environment.backendHost+"/accounts/"+accountId+"/pageOperations?page="+page+"&size="+size);
+
   }
 }
