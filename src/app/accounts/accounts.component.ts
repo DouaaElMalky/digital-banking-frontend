a0ad48bae220df791b5e064e9e5730a52c66adc2
Douaa El Malky <douaaelmalky3@gmail.com>
2024-12-31 12:23:32 +0100

    Commit

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {AccountsService} from '../services/accounts.service';
+import {Observable} from 'rxjs';
+import {AccountDetails} from '../model/account.model';
 
 @Component({
   selector: 'app-accounts',
@@ -10,15 +13,36 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class AccountsComponent implements OnInit{
   accountFormGroup! : FormGroup;
-  constructor(private fb : FormBuilder) {
+  currentPage : number = 0;
+  pageSize : number = 5;
+  accountObservable! : Observable<AccountDetails>
+  // account$, le $ veut dire Observable, ce qui veut dire que si on veut qlq chose de lui faudra faire subscribe
+  operationFormGroup!: FormGroup;
+  constructor(private fb : FormBuilder, private accountService : AccountsService) {
   }
   ngOnInit(): void {
     this.accountFormGroup = this.fb.group({
-      accoundId : this.fb.control('')
-    })
+      accountId : this.fb.control('')
+    });
+    this.operationFormGroup = this.fb.group({
+      operationType : this.fb.control(null),
+      amount : this.fb.control(0),
+      description : this.fb.control(null),
+      accountDestination : this.fb.control(null),
+    });
   }
 
   handleSearchAccount() {
+    let accountId : string = this.accountFormGroup.value.accountId;
+    this.accountObservable = this.accountService.getAccount(accountId, this.currentPage, this.pageSize);
+  }
+
+  gotoPage(page: number) {
+    this.currentPage = page;
+    this.handleSearchAccount();
+  }
+
+  handleAccountOperation() {
 
   }
 }
